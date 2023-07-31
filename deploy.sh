2925870f06b2c6fddde349a6a303b2f0cea2c5c0
Abhishek Bodhekar <abhishekbodhekar@Abhisheks-MacBook-Pro.local>
2023-07-31 16:29:15 +0530

    bigg

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+export changes=$(git diff HEAD~1 --name-only | xargs dirname | sort | uniq)
+for b in $changes 
+  do
+  echo "------"
+  file="./${b}/version.yaml"
+  if [ -f "$file" ]; then echo $b 
+  repo=$(yq '.repo' $file)
+  chart=$(yq '.chart' $file)
+  version=$(yq '.version' $file)
+  release=$(yq '.release' $file)
+  helm repo add "${b}-repo" $repo
+  helm repo update "${b}-repo"
+
+  file2="./${b}/values.yaml"
+  if [ -f "$file2" ]; 
+  then 
+    helm upgrade --install $release "${b}-repo"/$chart --version=$version -f $file2
+  else
+    helm upgrade --install $release "${b}-repo"/$chart --version=$version
+  fi
+  fi
+done
